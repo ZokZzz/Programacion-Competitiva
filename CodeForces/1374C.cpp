7cfc044ff7b232a488b5f29cb8f44e9ca0290153
David <davidss231006@gmail.com>
2025-11-29 21:16:36 -0500

    sosa

--- CodeForces/1374C.cpp
+++ CodeForces/1374C.cpp
@@ -1,21 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-ios_base::sync_with_stdio(0);
-cin.tie(nullptr);
-int t;
-cin>>t;
-while(t-->0){
-int n, c=0;
-cin>>n;
-string str;
-cin>>str;
-stack<char> l, r;
-for(int i=0; i<n; i++){
-    if(str[i]=='(')
-}//for
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
 
 
-}//while
-return 0;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t = 0;
+    cin >> t;
+
+    while(t-->0){
+
+        int n = 0;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        stack<char> sorner;
+
+        int c = 0;
+
+        for(int i = 0; i < n; i++){
+
+            if(s[i] == '(' ) {
+
+               sorner.push('(');
+
+            }
+
+
+            if(s[i] == ')' && !(sorner.empty())) {
+
+                    sorner.pop();
+                    c++;
+
+
+            }
+
+
+
+        }
+
+
+
+        cout << (n/2) - c << "\n";
+
+
+    }
+
+
+
+
+    return 0;
 }
