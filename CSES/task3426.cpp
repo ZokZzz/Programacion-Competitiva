c2ed31c4d15a99dcb2dcf80a094892e1e2758010
David <davidss231006@gmail.com>
2025-12-15 18:21:40 -0500

    cses

---
+++ CSES/task3426.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    long long n = 0, k = 0;
+    cin >> n >> k;
+
+    long long x = 0, a = 0, b = 0, c = 0;
+    cin >> x >> a >> b >> c;
+
+    vector<long long> arr (n), xr;
+
+    arr[0] = x;
+
+    for(int i = 1; i < n; i++) {
+
+       arr[i] = ((a * (arr[i-1])) + b) % c;
+
+    }
+
+    long long r = 0;
+
+    for(int i = 0, j = i; i < n - k + 1; i++) {
+
+        while(j < n && j - i < k){
+
+            r ^= arr[j];
+            j++;
+
+        }
+
+        xr.pb(r);
+        r ^= arr[i];
+
+    }
+
+    long long ans = 0;
+
+    for(auto i : xr) ans ^= i;
+
+    cout << ans << "\n";
+
+
+    return 0;
+}
+
+
+
